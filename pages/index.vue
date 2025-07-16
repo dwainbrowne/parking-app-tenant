6720bd5297372fd9084fe0edf79d3a3d27b13300
Dwain Browne <dwain@snapsuite.io>
2025-07-16 01:49:57 -0400

    testing deployment

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2v0a2 2 0 01-2-2v-2a2 2 0 00-2-2H8z" />
             </svg>
           </div>
-          <h1 class="text-2xl font-bold text-gray-900 mb-2">Request Parking Request</h1>
+          <h1 class="text-2xl font-bold text-gray-900 mb-2">Request Parking Testing</h1>
           <p class="text-gray-600">Get temporary parking access for your visit</p>
         </div>
         
